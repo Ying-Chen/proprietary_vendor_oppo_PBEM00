37f3c47e306b6b501ab3f83329189326876d30e4
WenHao2130 <wenhao2130@outlook.com>
2026-01-03 15:16:40 +0800

    PBEM00: Sync dt commit 094517815e023b0b9c180eeef9b1404c5f89a84f
    Change-Id: I1bc85a1802f325ba14460fd6aed9130853fb2452
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- PBEM00-vendor.mk
+++ PBEM00-vendor.mk
@@ -192,7 +192,6 @@ PRODUCT_COPY_FILES += \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/dataqti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dataqti.rc \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/dpmQmiMgr.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dpmQmiMgr.rc \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/hw/init.oppo.sensor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.oppo.sensor.rc \
-    vendor/oppo/PBEM00/proprietary/vendor/etc/init/hw/init.oppo.vendor.motor.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.oppo.vendor.motor.rc \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/ims_rtp_daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/ims_rtp_daemon.rc \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/imsdatadaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/imsdatadaemon.rc \
     vendor/oppo/PBEM00/proprietary/vendor/etc/init/imsqmidaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/imsqmidaemon.rc \
